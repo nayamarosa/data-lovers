bd6c17aec2b78ba75441105ed012feab5b4ab3b6
nayamarosa <falecom@nayama.com.br>
2019-03-09 19:58:25 -0300

    Fiz tudo de novo do zero. Código js com filter funcionando no console.log e começo do HTML

--- src/main.js
+++ src/main.js
@@ -1,95 +1,100 @@
-let category;
-let year;
-
-async function getData(){
-  fetch('./data/injuries/injuries.json').then(response => {
-    return response.json();
-  }).then(data => {
-    function getCategory(){
-      let categorySelect = document.getElementsByName("categories");
-      for (let i = 0; i<categorySelect.length; i++){
-        categorySelect[i].addEventListener('click', function(){
-          category = categorySelect[i].value;
-          console.log(category)
-        })
-      }
-    }
-    
-    function getYear(){
-      let yearSelect = document.getElementsByName("years");
-      for (let i = 0; i < yearSelect.length; i++){
-        yearSelect[i].addEventListener('click', function(){
-          year = yearSelect[i].value;
-          console.log(year)   
-        });
-      }
-    }
-    
-    function btnReturn(){
-      let continueBtn = document.querySelector("#continue");
-      continueBtn.addEventListener('click', function(){
-        showInjuries();
-      })
-    }
-    
-    function filterYear(years){
-      if (year === "all-years"){
-        let allYears = (years["Year"] >= "2000-01-04" && years["Year"] <= "2015-01-04");
-        return allYears;
-      } else if (year === "year-00-04"){
-        let firstYearInterval = (years["Year"] >= "2000-01-04" && years["Year"] <= "2004-01-04");
-        return firstYearInterval;
-      } else if (year === "year-05-09"){
-        let secondYearInterval = (years["Year"] >= "2005-01-04" && years["Year"] <= "2009-01-04");
-        return secondYearInterval;
-      } else if (year === "year-10-15"){
-        let thridYearInterval = (years["Year"] >= "2010-01-04" && years["Year"] <= "2015-01-04");
-        return thridYearInterval;
-      }
-    }
-    
-    function filterCategoryInjuries(){
-      data.filter(function(injuries){
-        if (category === "car"){
-          let carInjurie = (injuries["Total_Injured_Persons_Passenger_Car_Occupants"]);
-          return carInjurie;
-        } else if (category === "bus"){
-          let busInjurie = (injuries["Total_Injured_Persons_Bus_Occupants"]);
-          return busInjurie;
-        } else if (category === "truck"){
-          let truckInjurie = (injuries["Total_Injured_Persons_Truck_Occupants_Larg"]);
-          return truckInjurie;
-        } else if (category === "moto"){
-          let motoInjurie = (injuries["Total_Injured_Persons_Motorcyclists"]);
-          return motoInjurie;
-        } else if (category === "bike"){
-          let bikeInjurie = (injuries["Total_Injured_Persons_Pedalcyclists"]);
-          return bikeInjurie;
-        }
-      });
-    }
-    
-    
-    function showInjuries(){
-      let jsonData = document.querySelector("#json-data");
-      jsonData.innerHTML = `
-      ${data.filter(filterYear).map((injurie) => `
-      <tr class="table-item">
-      <td>Pessoas acidentadas de carro</td>
-      <td>${injurie["Year"]}</td>
-      <td>${injurie["Total_Injured_Persons_Passenger_Car_Occupants"]}</td>
-      </tr>
-      `).join("")}
-      `
-    }
-    
-    // console.log(data);
-    getCategory()
-    getYear()
-    filterCategoryInjuries()
-    // filterYear()
-    btnReturn()
-  })
+function dataForm(){
+  let form = document.querySelector("#data-form");
+  // let filterType = document.getElementsByName("filter");
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('tá certo')
+  });
 }
 
-getData()
+function showSelect(){
+  let filterType = document.getElementsByName("filter");
+  for (let filter of filterType){
+    filter.addEventListener('click', function(){
+      let filterValue = filter.value;
+      let selectCategory = document.getElementById("select-category");
+      let selectYear = document.getElementById("select-year");
+      console.log(filterValue)
+      if (filterValue === "category"){
+        selectCategory.classList.remove("hidden");
+        selectYear.classList.add("hidden");
+      } else {
+        selectYear.classList.remove("hidden");
+        selectCategory.classList.add("hidden");
+      }
+    });
+  }
+}
+
+function userSelection(selectionList){
+  for (let select of selectionList){
+    select.addEventListener('click', function(){
+      let selectValue = select.value;
+      console.log(selectValue);
+      if(selectionList[0].name === "select-year"){
+        getDataYear(selectValue);
+        console.log("user year")
+      } else {
+        getDataCategory(selectValue);
+        console.log("user category")
+      }
+    });
+  }
+}
+
+function getDataYear(selection){
+  fetch('./data/injuries/injuries.json')
+  .then(response => { return response.json()})
+  .then(data => filterYear(data, selection));
+  console.log("ok data")
+}
+
+function filterYear(data, yearSelected){
+  console.log("ok")
+  console.log("filter")
+  data.filter(data => {
+    if(data.Year === yearSelected){
+      return data;
+    }
+  }).map(data => {
+    console.log("Carro " + data["Total_Injured_Persons_Passenger_Car_Occupants"]);
+    console.log("Ônibus " + data["Total_Injured_Persons_Bus_Occupants"]);
+    console.log("Caminhão " + data["Total_Injured_Persons_Truck_Occupants_Large"]);
+    console.log("Moto " + data["Total_Injured_Persons_Motorcyclists"]);
+    console.log("Bicicleta " + data["Total_Injured_Persons_Pedalcyclists"]);
+  });
+}
+
+function getDataCategory(selection){
+  fetch('./data/injuries/injuries.json')
+  .then(response => { return response.json()})
+  .then(data => filterCategory(data, selection));
+  console.log("ok data")
+}
+
+function filterCategory(data, categorySelected){
+  data.filter(data =>{
+    return data;
+  }).map(data =>{
+    if(data["Year"] === "2011-01-04"){
+      console.log("2011 " + data[categorySelected])
+    }
+    if(data["Year"] === "2012-01-04"){
+      console.log("2012 " + data[categorySelected])
+    }
+    if(data["Year"] === "2013-01-04"){
+      console.log("2013 " + data[categorySelected])
+    }
+    if(data["Year"] === "2014-01-04"){
+      console.log("2014 " + data[categorySelected])
+    }
+    if(data["Year"] === "2015-01-04"){
+      console.log("2015 " + data[categorySelected])
+    }
+  });
+}
+
+userSelection(document.getElementsByName("select-category"));
+userSelection(document.getElementsByName("select-year"))
+dataForm()
+showSelect()
--- src/oldmain.js
+++ src/oldmain.js
@@ -5,7 +5,7 @@ function getCategory(){
   let categorySelect = document.getElementsByName("categories");
   for (let i = 0; i < categorySelect.length; i++){
     categorySelect[i].addEventListener('click', function(){
-      let category = categorySelect[i].value;
+      category = categorySelect[i].value;
       console.log(category)
     });
   }
@@ -15,54 +15,50 @@ function getYear(){
   let yearSelect = document.getElementsByName("years");
   for (let i = 0; i < yearSelect.length; i++){
     yearSelect[i].addEventListener('click', function(){
-      let year = yearSelect[i].value;
+      year = yearSelect[i].value;
       console.log(year)   
     });
   }
 }
 
-function getData(){
+async function getData(){
   fetch('./data/injuries/injuries.json').then(response => {
     return response.json();
   }).then(data => {
     
-    function filterYear(years){
-      if (year === "all-years"){
-        let allYears = (years["Year"] >= "2000-01-04" && years["Year"] <= "2015-01-04");
-        console.log(allYears)
-        return allYears;
-      } else if (year === "year-00-04"){
-        let firstYearInterval = (years["Year"] >= "2000-01-04" && years["Year"] <= "2004-01-04");
-        return firstYearInterval;
-      } else if (year === "year-05-09"){
-        let secondYearInterval = (years["Year"] >= "2005-01-04" && years["Year"] <= "2009-01-04");
-        return secondYearInterval;
-      } else if (year === "year-10-15"){
-        let thridYearInterval = (years["Year"] >= "2010-01-04" && years["Year"] <= "2015-01-04");
-        return thridYearInterval;
-      } 
+    function filterYear(){
+      data.filter(function(years){
+        if (year === "all-years"){
+          let allYears = (years["Year"] >= "2000-01-04" && years["Year"] <= "2015-01-04");
+          return allYears;
+        } else if (year === "year-00-04"){
+          let firstYearInterval = (years["Year"] >= "2000-01-04" && years["Year"] <= "2004-01-04");
+          return firstYearInterval;
+        } else if (year === "year-05-09"){
+          let secondYearInterval = (years["Year"] >= "2005-01-04" && years["Year"] <= "2009-01-04");
+          return secondYearInterval;
+        } else if (year === "year-10-15"){
+          let thridYearInterval = (years["Year"] >= "2010-01-04" && years["Year"] <= "2015-01-04");
+          return thridYearInterval;
+        } 
+      }).map(filterCategoryInjuries);
     }
     
     function filterCategoryInjuries(injuries){
       if (category === "car"){
-        let carInjurie = (injuries["Total_Injured_Persons_Passenger_Car_Occupants"]);
-        console.log(carInjurie)
+        let carInjurie = (injuries.Total_Injured_Persons_Passenger_Car_Occupants);
         return carInjurie;
       } else if (category === "bus"){
         let busInjurie = (injuries["Total_Injured_Persons_Bus_Occupants"]);
-        console.log(busInjurie)
         return busInjurie;
       } else if (category === "truck"){
         let truckInjurie = (injuries["Total_Injured_Persons_Truck_Occupants_Large"]);
-        console.log(truckInjurie)
         return truckInjurie;
       } else if (category === "moto"){
         let motoInjurie = (injuries["Total_Injured_Persons_Motorcyclists"]);
-        console.log(motoInjurie)
         return motoInjurie;
       } else if (category === "bike"){
         let bikeInjurie = (injuries["Total_Injured_Persons_Pedalcyclists"]);
-        console.log(bikeInjurie)
         return bikeInjurie;
       }
     }
@@ -77,15 +73,18 @@ function getData(){
     function showInjuries(){
       let jsonData = document.querySelector("#json-data");
       jsonData.innerHTML = `
-      ${data.filter(filterYear).filter(filterCategoryInjuries).map((injurie) => `
+      ${data.filter(filterYear).map((injurie) => `
       <tr class="table-item">
       <td>lalala</td>
-      <td></td>
-      <td>${filterCategoryInjuries()}</td>
+      <td>${injurie.year}</td>
+      <td>${injurie.category}</td>
       </tr>
       `).join("")}
       `
+      
+      console.log(data.filter(filterYear))
     }
+    
     
     
     // console.log(data);
@@ -99,3 +98,20 @@ function getData(){
 
 getData()
 
+
+function pega(){
+  fetch("./injuries.json")
+  .then(response => response.json())
+  .then(data => air(data));
+}
+
+function air(data){
+  data.filter(data =>{
+    if(data.Year === year){
+      return data;
+    }
+  }).map(data => {
+    console.log(data[category]);
+    print(data);
+  })
+}
